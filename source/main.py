b5b112a6817b3add4b948aecd5caddd258c3f0c9
amirhosein-kia-darbandsary <amirhoseinsavedarbandsary@gmail.com>
2023-03-29 20:50:19 +0330

    add log folder and artifact action

--- source/main.py
+++ source/main.py
@@ -1,8 +1,11 @@
 def main():
     answre = 2 + 2
+    with open('./dist/log.txt'  , 'w') as f:
+        f.write("this is a log test")
+        f.close()
     return answre
 
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
