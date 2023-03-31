f64af68aa26c6ffb65d88e0cca9557fcab9a3285
amirhosein-kia-darbandsary <amirhoseinsavedarbandsary@gmail.com>
2023-03-31 09:09:29 +0330

    change requirements.txt fiel

--- tests/test_env.py
+++ tests/test_env.py
@@ -5,7 +5,7 @@ class TestEnv(unittest.TestCase):
 
     def test_env(self):
         name = read_logger_name()
-        expected =  os.environ['USER_ACTION']
+        expected =  os.environ['USER_GIT']
         print(os.environ['TESTING_PACKAGE'])
         self.assertEqual(name,)
 
